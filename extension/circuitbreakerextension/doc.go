5b7ebc71939281fa3ea00505c48e289090b28f12
sergeysedoy97 <48452323+sergeysedoy97@users.noreply.github.com.>
2026-09-02 22:11:49 +0300

    [extension/circuitbreaker] add

---
+++ extension/circuitbreakerextension/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate make mdatagen
+
+package circuitbreakerextension // import "github.com/sergeysedoy97/opentelemetry-collector-contrib/extension/circuitbreakerextension"
