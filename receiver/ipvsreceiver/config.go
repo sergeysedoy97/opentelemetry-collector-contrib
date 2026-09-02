d074cd2aca6c0631b73a75ca6689e41d1a580eb8
sergeysedoy97 <48452323+sergeysedoy97@users.noreply.github.com.>
2026-09-02 22:06:59 +0300

    [receiver/ipvs] add

---
+++ receiver/ipvsreceiver/config.go
@@ -0,0 +1,18 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package ipvsreceiver // import "github.com/sergeysedoy97/opentelemetry-collector-contrib/receiver/ipvsreceiver"
+
+import (
+	"github.com/sergeysedoy97/opentelemetry-collector-contrib/receiver/ipvsreceiver/internal/metadata"
+	"go.opentelemetry.io/collector/scraper/scraperhelper"
+)
+
+type Config struct {
+	scraperhelper.ControllerConfig `mapstructure:",squash"`
+	metadata.MetricsBuilderConfig  `mapstructure:",squash"`
+}
+
+func (c *Config) Validate() error {
+	return c.ControllerConfig.Validate()
+}
